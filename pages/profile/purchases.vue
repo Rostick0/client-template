292136b3a03da28c55f0527b409b0b7e42bbcb5c
Rostislav <rostik057@gmail.com>
2024-11-03 11:45:04 +0300

    +add periods for chart in purchases

--- pages/profile/purchases.vue
+++ pages/profile/purchases.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="profile-purchases">
-    <div class="">{{ data }}</div>
+    <!-- <div class="">{{ data }}</div> -->
     <VFormComponent :field="select" />
     <div class="profile-purchases__charts">
       <template v-if="select.modelValue.id === 'DATE'">
@@ -10,7 +10,7 @@
               {
                 backgroundColor: 'rgb(216, 227, 252)',
                 borderColor: 'rgb(48, 113, 231)',
-                label: 'Продажи',
+                label: 'Покупки',
                 fill: true,
                 data: sumTotals,
               },
@@ -33,6 +33,36 @@
           />
         </div>
       </template>
+      <template v-if="select.modelValue.id === 'WEEK'">
+        <div class="profile-purchases__chart" v-if="sumTotals?.length">
+          <UiChartStats
+            :datasets="[
+              {
+                backgroundColor: 'rgb(216, 227, 252)',
+                borderColor: 'rgb(48, 113, 231)',
+                label: 'Покупки',
+                fill: true,
+                data: sumTotals,
+              },
+            ]"
+            :labels="weeksLables"
+          />
+        </div>
+        <div class="profile-purchases__chart" v-if="sumOrderings?.length">
+          <UiChartStats
+            :datasets="[
+              {
+                backgroundColor: 'rgb(216, 227, 252)',
+                borderColor: 'rgb(48, 113, 231)',
+                label: 'Количество заказов',
+                fill: true,
+                data: sumOrderings,
+              },
+            ]"
+            :labels="weeksLables"
+          />
+        </div>
+      </template>
       <template v-if="select.modelValue.id === 'MONTH'">
         <div class="profile-purchases__chart" v-if="sumTotals?.length">
           <UiChartStats
@@ -40,7 +70,7 @@
               {
                 backgroundColor: 'rgb(216, 227, 252)',
                 borderColor: 'rgb(48, 113, 231)',
-                label: 'Продажи',
+                label: 'Покупки',
                 fill: true,
                 data: sumTotals,
               },
@@ -68,7 +98,6 @@
 </template>
 
 <script setup>
-import moment from "moment";
 import debounce from "lodash/debounce";
 
 const periods = [
@@ -83,33 +112,18 @@ const select = ref({
   modelValue: periods[0],
 
   bind: {
+    label: "Выборка по периодам",
     options: periods,
   },
 });
 
-const lastDaysOfYear = [...new Array(365)].map((i, idx, arr) => {
-  const curDate = moment()
-    .startOf("day")
-    .subtract(arr.length - 1 - idx, "days")
-    .format("YYYY-MM-DD");
+const days = getDaysPast();
+const daysLabels = days.map((item) => item.value);
 
-  return {
-    id: curDate,
-    value: moment(curDate).format("MM.DD.YY"),
-  };
-});
+const weeks = getWeeksPast();
+const weeksLables = weeks.map((item) => item.value);
 
-const days = lastDaysOfYear;
-const daysLabels = lastDaysOfYear.map((item) => item.value);
-
-const weeks = [];
-
-const months = [...new Array(12)].map((_, i) => ({
-  id: i + 1,
-  value: new Date(`${i + 1}`).toLocaleDateString("ru", {
-    month: "short",
-  }),
-}));
+const months = getMonthsPast();
 const monthsLables = months.map((item) => item.value);
 
 const { filters } = useFilters({
@@ -132,16 +146,20 @@ const setData = (periodDates) => {
   sumTotals.value = [];
   sumOrderings.value = [];
 
-  console.log(data.value);
   periodDates?.forEach((item) => {
-    // console.log(item);
     const period = data.value.find((el) => el?.date == item.id);
-    // console.log()
+
     sumTotals.value.push(period?.sum_total ?? 0);
     sumOrderings.value.push(period?.sum_orderings ?? 0);
   });
 };
 setData(days);
+
+const typeDate = {
+  DATE: days,
+  WEEK: weeks,
+  MONTH: months,
+};
 
 watch(
   () => select.value.modelValue,
@@ -149,17 +167,13 @@ watch(
     filters.value.period = cur.id;
     await get();
 
-    if (cur.id === "DATE") {
-      setData(days);
-    }
+    const typeFinded = typeDate[cur.id];
 
-    if (cur.id === "MONTH") {
-      setData(months);
-    }
-
-    // console.log(filters.value);
+    if (typeFinded) setData(typeFinded);
   }, 500)
 );
+
+// const select = {}
 
 definePageMeta({
   layout: "profile",
--- utils/date.js
+++ utils/date.js
@@ -41,3 +41,57 @@ export const isCurrentYear = (date) =>
 
 export const dateTimeUsabilityFormat = (date) =>
   moment(date).format("DD " + (isCurrentYear(date) ? "MMMM" : "MM YY"));
+
+export const getDaysPast = (count = 366) =>
+  [...new Array(count)].map((_, idx, arr) => {
+    const curDate = moment()
+      .startOf("day")
+      .subtract(arr.length - 1 - idx, "days")
+      .format("YYYY-MM-DD");
+
+    return {
+      id: curDate,
+      value: moment(curDate).format("MM.DD.YY"),
+    };
+  });
+
+export const getWeeksPast = (count = 366) => {
+  const weeks = [];
+  const endDate = moment();
+  const startDate = moment().subtract(count, "days");
+
+  // Устанавливаем дату на первый понедельник в диапазоне
+  const firstMonday = startDate.clone().day(1);
+  if (firstMonday.isBefore(startDate)) {
+    firstMonday.add(7, "days"); // Если первый понедельник уже прошел, переходим к следующему
+  }
+
+  const currentWeekStart = firstMonday;
+  let index = 1;
+
+  while (
+    currentWeekStart.isBefore(endDate) ||
+    currentWeekStart.isSame(endDate, "day")
+  ) {
+    const weekEnd = currentWeekStart.clone().add(6, "days"); // Конец недели
+
+    weeks.push({
+      id: index,
+      value: weekEnd.format("MM.DD.YY"),
+    });
+
+    // Переходим к следующей неделе
+    currentWeekStart.add(7, "days");
+    index++;
+  }
+
+  return weeks;
+};
+
+export const getMonthsPast = (count = 12) =>
+  [...new Array(count)].map((_, i) => ({
+    id: i + 1,
+    value: new Date(`${i + 1}`).toLocaleDateString("ru", {
+      month: "short",
+    }),
+  }));
